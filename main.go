2f40f9498e19d269b30bde437a0f3751534d15d6
Karl Skewes <karl.skewes@gmail.com>
2023-05-23 12:05:44 +1200

    feat: Graceful shutdown upon signal
    Gracefully shutdown the application upon receiving a signal.
    This could be "ctrl+c" on the command line or a signal from an orchestration
    system like systemd, container runtimes, etc.
    
    This requires running the HTTP server in it's own goroutine which
    slightly complicates testing.
    
    The grace periods for handling in-transit HTTP requests and active connections
    are both hard coded as constants for now.

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	httpShutdownPreStopDelaySeconds = 1
+	httpShutdownTimeoutSeconds      = 1
 )
 
 func main() {
@@ -10,9 +22,47 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.NotFoundHandler())
+	srv := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
 
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	var group errgroup.Group
+
+	group.Go(func() error {
+		<-ctx.Done()
+
+		log.Print("received OS signal to shutdown, use Ctrl+C again to force")
+
+		// reset signals so ctrl+c can be captured again
+		stop()
+
+		// before shutting down the HTTP server wait for any HTTP requests that are
+		// in transit on the network. Common in Kubernetes and other distributed
+		// systems.
+		time.Sleep(httpShutdownPreStopDelaySeconds * time.Second)
+
+		// give active connections time to complete or disconnect before closing.
+		ctx2, cancel := context.WithTimeout(ctx, httpShutdownTimeoutSeconds*time.Second)
+		defer cancel()
+
+		return srv.Shutdown(ctx2)
+	})
+
+	group.Go(func() error {
+		err := srv.ListenAndServe()
+		// http.ErrServerClosed is expected at shutdown.
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+
+		return err
+	})
+
+	if err := group.Wait(); err != nil {
 		log.Print(err)
 	}
 }
